b747086a1e4685f547708b73d9b2e30c1398cbf5
Lupita <lups1507@gmail.com>
2016-06-13 20:10:55 -0700

    commit from the class

---
+++ routes/lupita.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lupita on 9/06/16.
+ */
+var express = require('express');
+var router = express.Router();
+var objeto = {nombre:'lupita', apellido:'chacon'};
+
+/* GET home page. */
+router.get('/chacon', function(req, res, next) {
+    res.send('Ola k ase');
+});
+
+router.get('/lupitaJade', function(req, res, next) {
+    res.render('lupita');
+});
+
+router.get('/myView', function(req, res, next) {
+    res.render('myView',objeto);
+});
+
+module.exports = router;
