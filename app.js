5dbe759a5a02ce6bd9ef8dfc0cffff4504f84602
larm18 <mtro.luismendez@gmail.com>
2016-06-13 20:24:06 -0700

    Config

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var lupita = require('./routes/lupita');
+var oscar = require('./routes/lupita');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/lupita', lupita);
+app.use('/oscar', oscar);
 
 
 // catch 404 and forward to error handler
